223d5ed4016346143746adff4e9d154dff52c57d
Eduardo Santos <santos.eduardothomaz@gmail.com>
2022-11-15 14:35:20 -0300

    chore(entity name): camel case

--- generator/entity/name.go
+++ generator/entity/name.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"strings"
+	"unicode"
 )
 
 type EntityName struct {
@@ -22,4 +23,13 @@ func (en EntityName) PascalCase() string {
 	noUnderscore := strings.ReplaceAll(en.name, "_", " ")
 	title := strings.Title(noUnderscore)
 	return strings.ReplaceAll(title, " ", "")
+}
+
+func (en EntityName) CamelCase() string {
+	pascal := en.PascalCase()
+	pascalRunes := []rune(pascal)
+	camelRunes := make([]rune, 0, len(pascal))
+	camelRunes = append(camelRunes, unicode.ToLower(pascalRunes[0]))
+	camelRunes = append(camelRunes, pascalRunes[1:]...)
+	return string(camelRunes)
 }
--- generator/entity/name_test.go
+++ generator/entity/name_test.go
@@ -41,3 +41,38 @@ func TestEntityName_PascalCase(t *testing.T) {
 		})
 	}
 }
+
+func TestEntityName_CamelCase(t *testing.T) {
+	type testCase struct {
+		it   string
+		in   entity.EntityName
+		want string
+	}
+
+	tc := []testCase{
+		{
+			it:   "should return PascalCase names as camelCase",
+			in:   entity.NewEntityName("PascalCase", "", ""),
+			want: "pascalCase",
+		},
+		{
+			it:   "should return camelCase name as camelCase",
+			in:   entity.NewEntityName("camelCase", "", ""),
+			want: "camelCase",
+		},
+		{
+			it:   "should return snake_case name as camelCase",
+			in:   entity.NewEntityName("snake_case", "", ""),
+			want: "snakeCase",
+		},
+	}
+
+	for _, c := range tc {
+		t.Run(c.it, func(t *testing.T) {
+			actual := c.in.CamelCase()
+			if c.want != actual {
+				utils.Error(t, c.want, actual)
+			}
+		})
+	}
+}
